f22bf0cfbad5f3a70aaa0208acc758cd7287dc36
Michelle Giraldo <michellegsld@gmail.com>
2020-07-20 19:27:34 -0400

    [Initial] Just test for boxes == None and it's length

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Task 0: Lockboxes
+"""
+
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened
+    """
+    if boxes == None or len(boxes) == 0:
+        return True
+
+    
