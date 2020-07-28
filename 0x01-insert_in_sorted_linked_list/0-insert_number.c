095d812c4e196ac80164fa566baf8beb3459291a
Michelle Giraldo <michellegsld@gmail.com>
2020-07-28 11:19:51 -0400

    [Initial] Add file that will insert a new node

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - Adds a node into a sorted list
+ * @head: The first node of the list
+ * @number: The number of the new node
+ *
+ * Return: Always 0.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *current = *head;
+	listint_t *new_node = NULL;
+	listint_t *previous = NULL;
+
+	while (current && number >= current->n)
+	{
+		previous = current;
+		current = current->next;
+	}
+
+	if (number < current->n)
+	{
+		new_node = malloc(sizeof(listint_t));
+		new_node->n = number;
+		new_node->next = current;
+		previous->next = new_node;
+	}
+
+	return (new_node);
+}
