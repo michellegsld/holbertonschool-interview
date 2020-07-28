c91bdfdb43a23d81c1088ade6548ae6da203ff29
Michelle Giraldo <michellegsld@gmail.com>
2020-07-28 11:22:25 -0400

    [Update] Check previous, enter statement in case node must be entered at end of linked list

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -21,12 +21,13 @@ listint_t *insert_node(listint_t **head, int number)
 		current = current->next;
 	}
 
-	if (number < current->n)
+	if (number >= previous->n)
 	{
 		new_node = malloc(sizeof(listint_t));
 		new_node->n = number;
 		new_node->next = current;
-		previous->next = new_node;
+		if (previous)
+			previous->next = new_node;
 	}
 
 	return (new_node);
