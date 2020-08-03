62dffe6bfc935a2ac0d1bbd3df44adbc2e1b48c7
Michelle Giraldo <michellegsld@gmail.com>
2020-08-03 12:42:18 -0400

    [Update] Add function prototypes for mandatory functions

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -22,6 +22,12 @@ typedef struct binary_tree_s
 } binary_tree_t;
 
 typedef struct binary_tree_s heap_t;
+
+/* 0-binary_tree_node.c */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+/* 1-heap_insert.c */
+heap_t *heap_insert(heap_t **root, int value);
+/* binary_tree_print.c */
 void binary_tree_print(const binary_tree_t *);	/* Not required for checker */
 
 #endif /* BINARY_TREES_H */
