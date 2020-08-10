bb6cf9e38c1d7946739ff68f561987b07bf9dd08
Michelle Giraldo <michellegsld@gmail.com>
2020-08-10 13:46:40 -0400

    [Fix] Check for pep8

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 # File: 0x03-minimum_operations/0-minoperations.py
 
+
 # This method returns how many factors are within n
 def totalFactors(n):
     total = 0
     for i in range(1, n + 1):
-       if n % i == 0:
-           total += 1
+        if n % i == 0:
+            total += 1
     return total
+
 
 # Calls totalFactors() in order to get amount and adds 1
 def minOperations(n):
     total = totalFactors(n)
-    return total + 1    # Testing if this works as is correct amount for main file
+    return total + 1
+    # Testing if this works as is correct amount for main file
