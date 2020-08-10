691c73f2d66ce3dd481db9e68868c840aa41fda6
Michelle Giraldo <michellegsld@gmail.com>
2020-08-10 13:54:09 -0400

    [Fix] Correct the way comments were written

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-# File: 0x03-minimum_operations/0-minoperations.py
+"""
+File:
+0x03-minimum_operations/0-minoperations.py
+"""
 
 
-# This method returns how many factors are within n
 def totalFactors(n):
+    """
+    This method returns how many factors are within n
+    """
     total = 0
     for i in range(1, n + 1):
         if n % i == 0:
@@ -11,8 +16,10 @@ def totalFactors(n):
     return total
 
 
-# Calls totalFactors() in order to get amount and adds 1
 def minOperations(n):
+    """
+    Calls totalFactors() in order to get amount and adds 1
+    """
     if n <= 1:
         return 0
     total = totalFactors(n)
