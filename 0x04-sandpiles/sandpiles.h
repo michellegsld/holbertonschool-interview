17bf243f9c790ee06fca1c0ef64f1c2d4276874e
Michelle Giraldo <michellegsld@gmail.com>
2020-08-17 14:01:43 -0400

    [Update] Add more _ to guard

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,5 +1,5 @@
-#ifndef SANDPILES_H
-#define SANDPILES_H
+#ifndef _SANDPILES_H_
+#define _SANDPILES_H_
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -8,4 +8,4 @@
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 void topple_grid(int grid[3][3], int i, int j);
 
-#endif /* SANDPILES_H */
+#endif /* _SANDPILES_H_ */
