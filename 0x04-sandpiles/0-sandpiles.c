baedffa102d82849c942933390d1f2ebc6ff197b
Michelle Giraldo <michellegsld@gmail.com>
2020-08-17 14:18:33 -0400

    [Test] Don't create any 0s in toppleLocations

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -27,7 +27,7 @@ void topple_grid(int grid[3][3], int i, int j)
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 	int i, j, flag;
-	int toppleLocations[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+	int toppleLocations[3][3];
 
 	for (i = 0; i < 3; i++)
 		for (j = 0; j < 3; j++)
