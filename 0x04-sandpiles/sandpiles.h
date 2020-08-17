db98e46239260c7d0fdad984c98529522e1cfcc5
Michelle Giraldo <michellegsld@gmail.com>
2020-08-17 13:33:11 -0400

    [Initial] Create header file that includes the main function I created

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -2,13 +2,9 @@
 #define SANDPILES_H
 
 #include <stdlib.h>
-#include <stdlib.h>
+#include <stdio.h>
 
 /* 0-sandpiles.c */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-
-/* 0-main.c */
-static void print_grid_sum(int grid1[3][3], int grid2[3][3]);
-static void print_grid(int grid[3][3]);
 
 #endif /* SANDPILES_H */
