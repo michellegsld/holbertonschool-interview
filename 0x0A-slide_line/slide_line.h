e3e1254b29777868960bf2534a4db8c40959abc2
Michelle Giraldo <michellegsld@gmail.com>
2020-10-09 16:47:56 -0400

    [Initial] Set up with what should be included, define SLIDE_RIGHT and SLIDE_LEFT, add prototype

---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,12 @@
+#ifndef _SLIDE_LINE_H_
+#define _SLIDE_LINE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int slide_line(int *line, size_t size, int direction);
+
+#define SLIDE_LEFT  0
+#define SLIDE_RIGHT 1
+
+#endif	/* _SLIDE_LINE_H_ */
