47cda56579248be380280146e85f323828f815f4
Michelle Giraldo <michellegsld@gmail.com>
2020-10-09 19:29:44 -0400

    [Update] Correct move_zeros into move_non_zeros as sliding all non-zeros over

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -1,6 +1,6 @@
 #include "slide_line.h"
 
-void move_zeros(int *line, size_t size, int direction);
+void move_non_zeros(int *line, size_t size, int direction);
 void shift_line(int *line, size_t size, int direction);
 
 /**
@@ -12,6 +12,7 @@ void shift_line(int *line, size_t size, int direction);
 void swap(int *line, size_t a, size_t b)
 {
 	int temp = line[a];
+
 	line[a] = line[b];
 	line[b] = temp;
 }
@@ -21,17 +22,17 @@ void swap(int *line, size_t a, size_t b)
  * @line: Pointer to an array of integers
  * @size: The number of elements in line
  * @direction: The direction to slide and merge
- * 
+ *
  * Return: 1 on success and 0 on failure
  */
 int slide_line(int *line, size_t size, int direction)
 {
 	if (!line || (direction != 0 && direction != 1))
 		return (0);
-	
+
 	if (size > 2) /* Is this rlly needed ? */
 	{
-		move_zeros(line, size, direction);
+		move_non_zeros(line, size, direction);
 		shift_line(line, size, direction);
 	}
 
@@ -39,25 +40,25 @@ int slide_line(int *line, size_t size, int direction)
 }
 
 /**
- * move_zeros - Moves all the zeros away from one side
+ * move_non_zeros - Moves all the zeros away from one side
  * @line: Array of integers
  * @size: The number of elements in line
  * @direction: Where to move zeros away from
  */
-void move_zeros(int *line, size_t size, int direction)
+void move_non_zeros(int *line, size_t size, int direction)
 {
 	size_t i, j;
 
-	/* Move zeros left */
-	for (i = 0; i < size && direction == 1; i++)
+	/* Moves non-zeros to the left */
+	for (i = 0; i < size && direction == 0; i++)
 		for (j = 0; j < size - i - 1; j++)
-			if (line[j] > 0)
+			if (line[j] == 0)
 				swap(line, j, j + 1);
 
-	/* Move zeros right */
-	for (i = size - 1; i > 0 && direction == 0; i--)
+	/* Moves non-zeros to the right */
+	for (i = size - 1; i > 0 && direction == 1; i--)
 		for (j = size - 1; j > 0; j--)
-			if (line[j] > 0)
+			if (line[j] == 0)
 				swap(line, j, j - 1);
 }
 
@@ -91,5 +92,5 @@ void shift_line(int *line, size_t size, int direction)
 		}
 	}
 
-	move_zeros(line, size, direction);
+	move_non_zeros(line, size, direction);
 }
