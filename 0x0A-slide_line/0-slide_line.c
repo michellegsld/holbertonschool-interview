4121361d47be985c1b8818030716cfca5f3d8dcf
Michelle Giraldo <michellegsld@gmail.com>
2020-10-09 16:48:31 -0400

    [Initial] Add plain edgecases

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,17 @@
+#include "slide_line.h"
+
+/**
+ * slide_line - Slides and merges arrays of integers
+ * @line: Pointer to an array of integers
+ * @size: The number of elements in line
+ * @direction: The direction to slide and merge
+ * 
+ * Return: 1 on success and 0 on failure
+ */
+int slide_line(int *line, size_t size, int direction)
+{
+	if(!line || (direction != 0 && direction != 1))
+		return (0);
+
+	return (1);
+}
