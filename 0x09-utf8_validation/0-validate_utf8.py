e0f56d31ba0266c06f44ac5a3ead5f8e4617b292
Michelle Giraldo <michellegsld@gmail.com>
2020-10-09 20:57:48 -0400

    [Initial] Try to convert numbers into binary and attempt to decode

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Task: 0. UTF-8 Validation
+File: 0-validate_utf8.py
+"""
+
+
+def validUTF8(data):
+    """
+    Returns either True or False
+    This depends upon if data is a valid UTF-8 encoding
+    """
+    for (int x in data)
+        try:
+            '0:08b'.format(x).decode("utf-8", "strict")
+        except:
+            return False
+    return True
