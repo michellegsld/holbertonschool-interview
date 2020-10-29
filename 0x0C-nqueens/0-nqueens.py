7dc9b9a063ead9e8df328e191dd48ddf78dbca62
Michelle Giraldo <michellegsld@gmail.com>
2020-10-28 21:56:36 -0400

    [Initial] Make base empty file to account for edge cases they provide

---
+++ 0x0C-nqueens/0-nqueens.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Task 0: N queens
+"""
+
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
+
+try:
+    n = int(sys.argv[1])
+except BaseException:
+    print("N must be a number")
+    exit(1)
+
+if n < 4:
+    print("N must be at least 4")
+    exit(1)
