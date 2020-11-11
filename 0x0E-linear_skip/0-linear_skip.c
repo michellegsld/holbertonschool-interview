c68b04aaa754d493eecd63e058b5e0faada321e9
Michelle Giraldo <michellegsld@gmail.com>
2020-11-11 18:41:17 -0500

    [Initial] Add basic file

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,16 @@
+#include "search.h"
+
+/**
+ * linear_skip - To find a specific value in a sorted skip list of ints.
+ * @list: Pointer to head of list to search.
+ * @value: The value to search for.
+ * 
+ * Return: The pointer to the value or NULL
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	if (!list)
+		return (NULL);
+
+	return (NULL);
+}
