c0062dc129c337704808b289682ea3a07e25ffe9
Michelle Giraldo <michellegsld@gmail.com>
2020-11-18 17:18:27 -0500

    [Update] Debug and simplify where needed

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,26 +2,19 @@
 
 const request = require('request');
 const movieId = process.argv[2];
-const url = `https://swapi.dev/api/films/${movieId}/`;
+const movieurl = `https://swapi.dev/api/films/${movieId}/`;
 
-request.get(url, (error, response, body) => {
-  if (error) {
+request.get(movieurl, (error, response, body) => {
+  if (error)
     return console.log(error);
-  }
 
-  const characterNames = {};
   const characterList = JSON.parse(body).characters;
 
   for (let i = 0; i < characterList.length; i++) {
-    request.get(url, (error, response, body) => {
-      if (error) {
+    request.get(characterList[i], (error, response, body) => {
+      if (error)
         return console.log(error);
-      }
-      characters[i] = (JSON.parse(response.body).name);
+      console.log(JSON.parse(response.body).name);
     });
-  }
-
-  for (const character of characterNames) {
-    console.log(character[i]);
   }
 });
