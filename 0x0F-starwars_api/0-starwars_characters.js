0a93840399e6ea3bd601b68bcf66b816cd061c8b
Michelle Giraldo <michellegsld@gmail.com>
2020-11-18 17:21:31 -0500

    [Fix] Follow semistandard conventions

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -5,15 +5,17 @@ const movieId = process.argv[2];
 const movieurl = `https://swapi.dev/api/films/${movieId}/`;
 
 request.get(movieurl, (error, response, body) => {
-  if (error)
+  if (error) {
     return console.log(error);
+  }
 
   const characterList = JSON.parse(body).characters;
 
   for (let i = 0; i < characterList.length; i++) {
     request.get(characterList[i], (error, response, body) => {
-      if (error)
+      if (error) {
         return console.log(error);
+      }
       console.log(JSON.parse(response.body).name);
     });
   }
