51793ac94ba40d5b53f49efc6ff02f7c54049e49
Michelle Giraldo <michellegsld@gmail.com>
2020-11-18 17:38:53 -0500

    [Update] Make sure is an async function to use await new Promise to make sure everything will stay in order with using resolve()

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,7 +4,7 @@ const request = require('request');
 const movieId = process.argv[2];
 const movieurl = `https://swapi.dev/api/films/${movieId}/`;
 
-request.get(movieurl, (error, response, body) => {
+request.get(movieurl, async function (error, response, body) {
   if (error) {
     return console.log(error);
   }
@@ -12,11 +12,14 @@ request.get(movieurl, (error, response, body) => {
   const characterList = JSON.parse(body).characters;
 
   for (let i = 0; i < characterList.length; i++) {
-    request.get(characterList[i], (error, response, body) => {
-      if (error) {
-        return console.log(error);
-      }
-      console.log(JSON.parse(response.body).name);
+    await new Promise(function (resolve, reject) {
+      request.get(characterList[i], (error, response, body) => {
+        if (error) {
+          return console.log(error);
+        }
+        console.log(JSON.parse(response.body).name);
+        resolve();
+      });
     });
   }
 });
