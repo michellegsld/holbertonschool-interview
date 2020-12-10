d8586549919ecbdd8517e75f10a90b52e6f8f85b
Michelle Giraldo <michellegsld@gmail.com>
2020-12-09 19:56:59 -0500

    [Initial] Add for just negative case

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,15 @@
+#include "search_algos.h"
+
+/**
+* advanced_binary - Searches for a value in a sorted array of ints
+* @array: Pointer to the first element of the array to search in
+* @size: The number of elements in array
+* @value: the value to search for
+*
+* Return: -1 or the value to search for
+*/
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	return (-1);
+}
