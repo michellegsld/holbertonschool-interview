dfc1afe8318ab4375ec3b7ebda4cc93e2fa69f13
Michelle Giraldo <michellegsld@gmail.com>
2021-03-18 21:45:26 -0400

    [Fix] Check if now an executable

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -5,17 +5,22 @@ Task 0: N queens
 
 import sys
 
+def nqueens(n):
+    
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
 
-try:
-    n = int(sys.argv[1])
-except BaseException:
-    print("N must be a number")
-    sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except BaseException:
+        print("N must be a number")
+        sys.exit(1)
 
-if n < 4:
-    print("N must be at least 4")
-    sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    nqueens(n)
