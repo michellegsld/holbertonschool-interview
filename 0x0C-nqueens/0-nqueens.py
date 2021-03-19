2c5e25a2067e72b612cd899383691278c533516d
Michelle Giraldo <michellegsld@gmail.com>
2021-03-18 21:36:30 -0400

    [Update] Make executable, add if__name__ == __main__

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -5,16 +5,18 @@ Task 0: N queens
 
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    exit(1)
+if __name__ == '__main__':
 
-try:
-    n = int(sys.argv[1])
-except BaseException:
-    print("N must be a number")
-    exit(1)
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
 
-if n < 4:
-    print("N must be at least 4")
-    exit(1)
+    try:
+        n = int(sys.argv[1])
+    except BaseException:
+        print("N must be a number")
+        exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
