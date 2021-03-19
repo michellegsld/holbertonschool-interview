e5ad7f91a1c082f62757237b943ad4396f9241c6
Michelle Giraldo <michellegsld@gmail.com>
2021-03-18 21:40:14 -0400

    [Fix] Undo last commit and fix way sys is implemented

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -5,18 +5,17 @@ Task 0: N queens
 
 import sys
 
-if __name__ == '__main__':
 
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        exit(1)
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
 
-    try:
-        n = int(sys.argv[1])
-    except BaseException:
-        print("N must be a number")
-        exit(1)
+try:
+    n = int(sys.argv[1])
+except BaseException:
+    print("N must be a number")
+    sys.exit(1)
 
-    if n < 4:
-        print("N must be at least 4")
-        exit(1)
+if n < 4:
+    print("N must be at least 4")
+    sys.exit(1)
