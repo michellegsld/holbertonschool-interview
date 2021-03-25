0c6f585ecb5fbb1f517bbbe403871eb034141eaf
Michelle Giraldo <michellegsld@gmail.com>
2021-03-25 16:24:46 -0400

    [Update] Add prototype for printing function

--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -25,5 +25,6 @@ typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s avl_t;
 
 avl_t *sorted_array_to_avl(int *array, size_t size);
+void binary_tree_print(const binary_tree_t *);
 
 #endif	/* _BINARY_TREES_H_ */
