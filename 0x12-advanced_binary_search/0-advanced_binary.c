81a6622f2658b91543b14658178df9477173e140
Michelle Giraldo <michellegsld@gmail.com>
2021-04-29 15:09:26 -0400

    [Fix] Remove un-necessary part of if statement that would not compare final 2 numebrs

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -39,7 +39,7 @@ int simple_binary(int *array, size_t left, size_t right, int value)
 		print_binary(array, left, right);
 		answer = left + (right - left) / 2;
 
-		if (array[answer] == value && array[answer - 1] == value)
+		if (array[answer] == value)
 		{
 			print_binary(array, left, right);
 			return (answer);
