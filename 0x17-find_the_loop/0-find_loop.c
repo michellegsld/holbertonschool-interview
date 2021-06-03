39b0727201d716e13ae5542bf94d3967f3e71c9d
Michelle Giraldo <michellegsld@gmail.com>
2021-06-03 13:46:05 -0400

    [Initial] Try to find the loop

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - Finds the loop in a linked list
+ * @head: A pointer to the first node of a linked list
+ *
+ * Return: The address of the node where the loop starts or NULL
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow = head;
+	listint_t *fast = head;
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (slow);
+	}
+	return (NULL);
+}
