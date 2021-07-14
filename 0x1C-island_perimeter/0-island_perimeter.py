f4d0f3f44a09a1f95119f95f67444f6cbf8dc5df
Michelle Giraldo <michellegsld@gmail.com>
2021-07-14 14:24:45 -0400

    [Initial] Add mandatory task and find perimeter of island

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+0-island_perimeter
+Task 0: Island Perimeter
+"""
+
+def island_perimeter(grid):
+    """
+    Returns the parameter of the island, defined by grid
+    """
+    total = 0
+
+    rows = len(grid)
+    columns = len(grid[0])
+
+    for r in range(rows):
+        for c in range(columns):
+            cell = grid[r][c]
+            total += 4
+            if r != 0 and grid[r-1][c] is 1:
+                total -= 1
+            if c != 0 and grid[r][c-1] is 1:
+                total -= 1
+            if r + 1 != rows and grid[r + 1][c] is 1:
+                total -= 1
+            if c + 1 != columns and grid[r][c + 1] is 1:
+                total -= 1
+
+    return total
