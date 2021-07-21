17053912e1dc43b486bea8fba1cb4e87b94d4490
Michelle Giraldo <michellegsld@gmail.com>
2021-07-21 12:09:37 -0400

    [Initial] Base file, returns 0

---
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_avl - Determines if a binary tree is an AVL Tree
+ * @tree: The binary tree to be checked
+ *
+ * Return: Either 1 or 0
+ */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (0);
+}
